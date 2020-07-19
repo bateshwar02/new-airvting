d8767202e10a1eb5819968e79a608802056867a8
Bateshwar <bateshwar.mishra@sapple.co.in>
2020-07-19 23:52:26 +0530

    add live streaming code

--- src/containers/Home/components/liveVideo.js
+++ src/containers/Home/components/liveVideo.js
@@ -50,39 +50,39 @@ function Video({ stream }) {
 
   useEffect(()=> {
     if(window){
-        var subscriber = new window.red5prosdk.RTCSubscriber();
-        // Initialize
-        subscriber.init({
-        protocol: 'ws',
-        port: 8081,
-        host: 'localhost',
-        app: 'live',
-        streamName: 'mystream',
-        rtcConfiguration: {
-            iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
-            iceCandidatePoolSize: 2,
-            bundlePolicy: 'max-bundle'
-        }, // See https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#RTCConfiguration_dictionary
-        mediaElementId: 'red5pro-subscriber',
-        subscriptionId: 'mystream' + Math.floor(Math.random() * 0x10000).toString(16),
-        videoEncoding: 'NONE',
-        audioEncoding: 'NONE'
-        })
-        .then(function(subscriber) {
-            console.log('subscriber ===', subscriber);
-        // `subcriber` is the WebRTC Subscriber instance.
-        return subscriber.subscribe();
-        })
-        .then(function(subscriber) {
-        // subscription is complete.
-        // playback should begin immediately due to
-        //   declaration of `autoplay` on the `video` element.
-        })
-        .catch(function(error) {
-        // A fault occurred while trying to initialize and playback the stream.
-        console.error(error)
-        });
-    }
+      var subscriber = new window.red5prosdk.RTCSubscriber();
+      // Initialize
+      subscriber.init({
+      protocol: 'ws',
+      port: 5080,
+      host: 'localhost',
+      app: 'live',
+      streamName: 'bnm-mystream',
+      rtcConfiguration: {
+          iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
+          iceCandidatePoolSize: 2,
+          bundlePolicy: 'max-bundle'
+      }, // See https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#RTCConfiguration_dictionary
+      mediaElementId: 'red5pro-subscriber',
+      subscriptionId: 'mystream' + Math.floor(Math.random() * 0x10000).toString(16),
+      videoEncoding: 'NONE',
+      audioEncoding: 'NONE'
+      })
+      .then(function(subscriber) {
+          console.log('subscriber ===', subscriber);
+      // `subcriber` is the WebRTC Subscriber instance.
+      return subscriber.subscribe();
+      })
+      .then(function(subscriber) {
+      // subscription is complete.
+      // playback should begin immediately due to
+      //   declaration of `autoplay` on the `video` element.
+      })
+      .catch(function(error) {
+      // A fault occurred while trying to initialize and playback the stream.
+      console.error(error)
+      });
+  }
 }, [])
 
   return (
--- src/containers/Live/index.js
+++ src/containers/Live/index.js
@@ -120,52 +120,52 @@ export function Live({ postData, userData }) {
 
   useEffect(() => {
     if(window) {
-        var publisher = new window.red5prosdk.RTCPublisher();
-        // Initialize
-        publisher.init({
-            protocol: 'ws',
-            port:8081,
-            host: 'localhost',
-            app: 'live',
-            streamName: 'mystream',
-            rtcConfiguration: {
-              iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
-              iceCandidatePoolSize: 2,
-              bundlePolicy: 'max-bundle'
-            }, // See https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#RTCConfiguration_dictionary
-            streamMode: 'live',
-            mediaElementId: 'red5pro-publisher',
-            bandwidth: {
-              audio: 56,
-              video: 512
-            },
-            mediaConstraints: {
-              audio: true,
-              video: {
-                // width: {
-                //   exact: 640
-                // },
-                height: {
-                //   exact: 480
-                exact: 560
-                },
-                frameRate: {
-                  min: 8,
-                  max: 24
-                }
+      var publisher = new window.red5prosdk.RTCPublisher();
+      // Initialize
+      publisher.init({
+          protocol: 'ws',
+          port:5080,
+          host: 'localhost',
+          app: 'live',
+          streamName: 'bnm-mystream',
+          rtcConfiguration: {
+            iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
+            iceCandidatePoolSize: 2,
+            bundlePolicy: 'max-bundle'
+          }, // See https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#RTCConfiguration_dictionary
+          streamMode: 'live',
+          mediaElementId: 'red5pro-publisher',
+          bandwidth: {
+            audio: 56,
+            video: 512
+          },
+          mediaConstraints: {
+            audio: true,
+            video: {
+              // width: {
+              //   exact: 640
+              // },
+              height: {
+              //   exact: 480
+              exact: 560
+              },
+              frameRate: {
+                min: 8,
+                max: 24
               }
             }
-          })
-          .then(function() {
-            // Invoke the publish action.
-            return publisher.publish();
-          })
-          .catch(function(error) {
-            // A fault occurred while trying to initialize and publish the stream.
-            console.error(error);
-          });
-          
-    }
+          }
+        })
+        .then(function() {
+          // Invoke the publish action.
+          return publisher.publish();
+        })
+        .catch(function(error) {
+          // A fault occurred while trying to initialize and publish the stream.
+          console.error(error);
+        });
+        
+  }
   
 }, []);
   const getContent = () => (
