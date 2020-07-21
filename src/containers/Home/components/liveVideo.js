131db72652692b4c313ec074d1ab5288c693b9fc
Bateshwar <bateshwar.mishra@sapple.co.in>
2020-07-21 16:51:49 +0530

    changes live config

--- src/containers/Home/components/liveVideo.js
+++ src/containers/Home/components/liveVideo.js
@@ -10,80 +10,42 @@ import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
 function Video({ stream }) {
-  // useEffect(() => {
-  //   if (window) {
-  //     const subscriber = new window.red5prosdk.RTCSubscriber();
-  //     // Initialize
-  //     subscriber.init({
-  //       protocol: 'ws',
-  //       port: 5080,
-  //       host: '52.77.219.22',
-  //       app: 'live',
-  //       streamName: stream,
-  //       rtcConfiguration: {
-  //         iceServers: [{ urls: 'stun:stun2.l.google.com:19302' }],
-  //         iceCandidatePoolSize: 2,
-  //         bundlePolicy: 'max-bundle'
-  //       },
-  //       mediaElementId: 'red5pro-subscriber',
-  //       subscriptionId: stream,
-  //       videoEncoding: 'NONE',
-  //       audioEncoding: 'NONE'
-  //     })
-  //       .then((subscribers) => {
-  //         console.log('subscriber ===', subscribers);
-  //         // `subcriber` is the WebRTC Subscriber instance.
-  //         return subscriber.subscribe();
-  //       })
-  //       .then((subs) => {
-  //         console.log(subs);
-  //         // subscription is complete.
-  //         // playback should begin immediately due to
-  //         //   declaration of `autoplay` on the `video` element.
-  //       })
-  //       .catch((error) => {
-  //         // A fault occurred while trying to initialize and playback the stream.
-  //         console.error(error);
-  //       });
-  //   }
-  // }, []);
-
   useEffect(()=> {
     if(window){
-      var subscriber = new window.red5prosdk.RTCSubscriber();
-      // Initialize
-      subscriber.init({
-      protocol: 'ws',
-      port: 5080,
-      host: 'localhost',
-      app: 'live',
-      streamName: 'bnm-mystream',
-      rtcConfiguration: {
-          iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
-          iceCandidatePoolSize: 2,
-          bundlePolicy: 'max-bundle'
-      }, // See https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#RTCConfiguration_dictionary
-      mediaElementId: 'red5pro-subscriber',
-      subscriptionId: 'mystream' + Math.floor(Math.random() * 0x10000).toString(16),
-      videoEncoding: 'NONE',
-      audioEncoding: 'NONE'
-      })
-      .then(function(subscriber) {
-          console.log('subscriber ===', subscriber);
-      // `subcriber` is the WebRTC Subscriber instance.
-      return subscriber.subscribe();
-      })
-      .then(function(subscriber) {
-      // subscription is complete.
-      // playback should begin immediately due to
-      //   declaration of `autoplay` on the `video` element.
-      })
-      .catch(function(error) {
-      // A fault occurred while trying to initialize and playback the stream.
-      console.error(error)
-      });
-  }
-}, [])
+        var subscriber = new window.red5prosdk.RTCSubscriber();
+        // Initialize
+        subscriber.init({
+        protocol: 'ws',
+        port: 5080,
+        host: 'localhost',
+        app: 'live',
+        streamName: 'bnm-test',
+        rtcConfiguration: {
+            iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
+            iceCandidatePoolSize: 2,
+            bundlePolicy: 'max-bundle'
+        }, // See https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#RTCConfiguration_dictionary
+        mediaElementId: 'red5pro-subscriber',
+        subscriptionId: 'mystream' + Math.floor(Math.random() * 0x10000).toString(16),
+        videoEncoding: 'NONE',
+        audioEncoding: 'NONE'
+        })
+        .then(function(subscriber) {
+            console.log('subscriber ===', subscriber);
+        // `subcriber` is the WebRTC Subscriber instance.
+        return subscriber.subscribe();
+        })
+        .then(function(subscriber) {
+        // subscription is complete.
+        // playback should begin immediately due to
+        //   declaration of `autoplay` on the `video` element.
+        })
+        .catch(function(error) {
+        // A fault occurred while trying to initialize and playback the stream.
+        console.error(error)
+        });
+    }
+}, []);
 
   return (
     <div className="videoPublishWrapper">
